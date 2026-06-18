2530b048ce58ca916f1f88702525d719fd3f3824
suraj shenoy <surajshenoyp@gmail.com>
2026-06-19 00:08:39 +0530

    ficxing error

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Point this to your running FastAPI backend.
-const API_BASE = "https://skill-swap-backend-cnqr.onrender.com/";
-const WS_BASE = "https://skill-swap-backend-cnqr.onrender.com/";
+const API_BASE = "https://skill-swap-backend-cnqr.onrender.com";
+const WS_BASE = "wss://skill-swap-backend-cnqr.onrender.com";
